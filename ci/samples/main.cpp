b876a029873abb9fb696cd14ba775054d4b89ad4
Corentin Le Molgat <corentinl@google.com>
2022-08-05 18:16:57 +0200

    Fix ci/samples

--- ci/samples/main.cpp
+++ ci/samples/main.cpp
@@ -4,13 +4,9 @@
 
 int main(int /*argc*/, char** /*argv*/) {
   squirrel3::freeFunction(0);
-  bar::freeFunction(1);
-  squirrel3bar::freeFunction(2);
   std::cout << std::endl;
 
-  squirrel3::squirrel3::staticFunction(int{0});
-  bar::Bar::staticFunction(int{1});
-  squirrel3bar::squirrel3Bar::staticFunction(int{2});
+  squirrel3::Squirrel3::staticFunction(int{0});
   std::cout << std::endl;
 
   return 0;
